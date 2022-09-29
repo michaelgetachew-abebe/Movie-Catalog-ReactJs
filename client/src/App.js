439283c947c3068c65fb3f5bdcb31188708579a1
michaelgetachew <mikygetyos@gmail.com>
2022-09-29 12:13:51 +0300

    Feat(add): started configuring the movie container section of the website

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,13 @@ import SearchIcon from './search.svg';
 
 
 const API_URL = 'http://www.omdbapi.com?apikey=51f209c2'
-
+const movie1 = {
+    Poster: "N/A",
+    Title: "Spiderman",
+    Type: "movie",
+    Year: "1990",
+    imdbID: "tt0100669"
+}
 const App = () => {
     const searchMovies = async (title) => {
         const response = await fetch(`${API_URL}&s=${title}}`);
@@ -25,7 +31,24 @@ const App = () => {
                 <input placeholder = "Search for movies" value = "Superman" onChange = {() => {}}/>
                 <img src = {SearchIcon} alt = "search" onClick = {() => {}}/>
             </div>
+            <div className = "container">
+                <div className = "movie">
+                    <div>
+                        <p>{movie1.Year}</p>
+                    </div>
+                    <div>
+                        <img src={movie1.Poster !== 'N/A' ? movie1.Poster : 'https://via.placeholder.com/400'} alt={movie1.Title}/>
+                    </div>
+                    <div>
+                        <span>{movie1.Type}</span>
+                        <h3>{movie1.Title}</h3>
+                    </div>
+                </div>            
+            </div>
+
         </div>
-    );
+
+            
+            );
 }
 export default App;
