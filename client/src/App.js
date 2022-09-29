8839d7bd5ded219dd908fce4af16828071da9b61
michaelgetachew <mikygetyos@gmail.com>
2022-09-29 12:54:05 +0300

    Feat(add): Finilizing the project

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,21 @@
-import { useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import './App.css';
+import MovieCard from './Moviecard';
 //API KEY: 51f209c2
 import SearchIcon from './search.svg';
 
 
 const API_URL = 'http://www.omdbapi.com?apikey=51f209c2'
-const movie1 = {
-    Poster: "N/A",
-    Title: "Spiderman",
-    Type: "movie",
-    Year: "1990",
-    imdbID: "tt0100669"
-}
+
 const App = () => {
+    const [movies, setMovies] = useState([]);
+    const [searchTerm, setSearchTerm] = useState('');
+
     const searchMovies = async (title) => {
         const response = await fetch(`${API_URL}&s=${title}}`);
         const data = await response.json()
 
-        console.log(data.Search);
+        setMovies(data.Search);
     }
     useEffect(() => {
         searchMovies('Spiderman')
@@ -28,12 +26,24 @@ const App = () => {
             <h1>MovieLand</h1>
 
             <div className = "search">
-                <input placeholder = "Search for movies" value = "Superman" onChange = {() => {}}/>
-                <img src = {SearchIcon} alt = "search" onClick = {() => {}}/>
+                <input placeholder = "Search for movies" value = {searchTerm} onChange = {(e) => setSearchTerm(e.target.value)}/>
+                <img src = {SearchIcon} alt = "search" onClick = {() => searchMovies(searchTerm)}/>
             </div>
-            <div className = "container">
-                            
-            </div>
+            {
+                movies?.length > 0 
+                    ? (
+                        <div className = "container">
+                            {movies.map((movie) => (
+                                <MovieCard movie={movie} />
+                            ))}                
+                        </div>
+                    ) : (
+                        <div>
+                            <h2>Opps, No Movies Right Now!!!</h2>
+                        </div>
+                    )
+            }
+            
 
         </div>
 
